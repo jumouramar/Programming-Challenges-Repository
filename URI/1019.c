43d15d65a05f02f164dc529dd52b7052e5870aca
Juliana Martins de Moura <juliana_moura@id.uff.br>
2021-06-02 19:58:29 -0300

    1019

---
+++ URI/1019.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int segundos, minutos, horas;
+    scanf("%d", &segundos);
+
+    minutos = segundos/60;
+    segundos-=minutos*60;
+    horas = minutos/60;
+    minutos -= horas*60;
+
+    printf("%d:%d:%d\n", horas, minutos, segundos);
+    return 0;
+}
