52e3c8a57c4ce5cdc3bc88e897df66ac1bb616b7
Juliana Martins de Moura <juliana_moura@id.uff.br>
2021-06-08 12:26:46 -0300

    1065

---
+++ URI/1065.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int valor, par=0;
+    for (int i = 0; i < 5; i++){
+        scanf("%d", &valor);
+        if (valor%2==0) par++;
+    }
+    printf("%d valores pares\n", par);
+
+    return 0;
+}
