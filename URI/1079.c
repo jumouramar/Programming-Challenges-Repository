e61ced81302021c6b24dff3900014c6e4c129103
Juliana Martins de Moura <juliana_moura@id.uff.br>
2021-06-09 11:50:34 -0300

    1079

---
+++ URI/1079.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    double a, b, c, media;
+    scanf("%d", &n);
+    for(int i=0; i<n; i++){
+        scanf("%lf %lf %lf", &a, &b, &c);
+        media = (a*2+b*3+c*5)/10;
+        printf("%.1lf\n", media);
+    }
+
+    return 0;
+}
