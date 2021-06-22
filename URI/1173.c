8cfb2ae2e03962b27b6e42553ea7f1e925eaca4d
Juliana Martins de Moura <juliana_moura@id.uff.br>
2021-06-22 08:19:36 -0300

    1173

---
+++ URI/1173.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int n[10], v;
+    scanf("%d", &v);
+    for (int i = 0; i < 10; i++){
+        n[i] = v;
+        v *= 2;
+        printf("N[%d] = %d\n", i, n[i]);
+    }
+
+    return 0;
+}
