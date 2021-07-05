c0fd7d380a3f2dcdfddaa324fe1a7b585817a143
Juliana Martins de Moura <juliana_moura@id.uff.br>
2021-07-05 15:03:48 -0300

    1924

---
+++ URI/1924.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int n;
+    char curso;
+    for(int i = 0; i<n; i++){
+        scanf("%s", curso);
+    }
+    printf("Ciencia da Computacao\n");
+
+    return 0;
+}
