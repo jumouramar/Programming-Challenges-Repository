2c413358f78c6f1abc8c8327250fe61f437321f3
Juliana Martins de Moura <juliana_moura@id.uff.br>
2021-07-08 08:28:48 -0300

    1962

---
+++ URI/1962.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int n, t;
+    scanf("%d", &n);
+    for(int i = 0; i<n; i++){
+        scanf("%d", &t);
+        if(t-2015>=0)
+            printf("%d A.C.\n", t-2014);
+        else
+            printf("%d D.C.\n", 2015-t);
+    }
+
+    return 0;
+}
