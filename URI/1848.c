a93c59bce4aa727f3353de2ff657c8e494d3eefa
Juliana Martins de Moura <juliana_moura@id.uff.br>
2021-07-08 10:21:45 -0300

    1848

---
+++ URI/1848.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main(){
+
+    char entrada[7];
+    int count = 3, resposta = 0;
+    while(count--){
+        while(1){
+            scanf("%s", entrada);
+            if(strcmp(entrada, "caw") == 0){
+                scanf("%s", entrada);
+                break;
+            }
+            if(entrada[0] == '*')
+                resposta+=4;
+            if(entrada[1] == '*')
+                resposta+=2;
+            if(entrada[2] == '*')
+                resposta+=1;
+        }
+        printf("%d\n", resposta);
+        resposta = 0;
+    }
+
+    return 0;
+}
