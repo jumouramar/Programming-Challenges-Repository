b8dd3e378f4abb23693c883c170d4fe42ffd2d21
Juliana Martins de Moura <juliana_moura@id.uff.br>
2021-07-15 18:52:55 -0300

    1837

--- URI/1837.c
+++ URI/1837.c
@@ -6,17 +6,30 @@ int main(){
     scanf("%d %d", &a, &b);
 
     if(a<0){
-        for(int i = 0; i<b; i++){
+        if(b<0){
+           b *= -1;
+            for(int i = 0; i<b; i++){
+                q = (a-i)/b;
+                r = a-b*q;
+                if(i == r) {
+                    q = -q;
+                    break;
+                }
+            } 
+        }
+        else{
+            for(int i = 0; i<b; i++){
             q = (a-i)/b;
             r = a-b*q;
             if(i == r) break;
+            }
         }
     }
     else{
         q = a/b;
         r = a%b;
     }
-    
+
     printf("%d %d\n", q, r); 
     
     return 0;
